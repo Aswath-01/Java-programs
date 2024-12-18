7aba3c4a2ead9ed552f5358daead341b2bb5e6a7
Aswath-01 <148414485+Aswath-01@users.noreply.github.com>
2024-12-18 21:32:00 +0530

    Var_Types.java

--- Var_Types.java
+++ Var_Types.java
@@ -6,7 +6,7 @@ public class Var_Types {
 static int a=10,c=12,d;
 String Greet="Hi !! This is a java program that implements types of variables.";//instance variable
 	public static void main(String[] args) {
-Scanner ic=new Scanner(System.in);
+//Scanner ic=new Scanner(System.in);
 Var_Types obj=new Var_Types();
 System.out.println(obj.Greet);
 Var_Types.add();
